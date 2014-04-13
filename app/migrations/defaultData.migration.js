2deb4d3a92340c94396eaa6768161f7ff349fdcb
Scott Brown <link1900@gmail.com>
2014-04-13 17:27:16 +1000

    Changed data migrations for group level. Added group level edit ui and fixed list ui

--- app/migrations/defaultData.migration.js
+++ app/migrations/defaultData.migration.js
@@ -10,8 +10,8 @@ migration.up = function(){
     return helper.saveAll([
         new AllowedUser({"email":"link1900@gmail.com"}),
         new AllowedUser({"email":"nbrown99@bigpond.net.au"}),
-        new GroupLevel({"_id" : "531d1f72e407586c21476ef7", "name" : "Group 1", "level":1}),
-        new GroupLevel({"_id" : "531d1f72e407586c21476f0c", "name" : "Group 2", "level":2}),
-        new GroupLevel({"_id" : "531d1f72e407586c21476f0d", "name" : "Group 3", "level":3})
+        new GroupLevel({"_id" : "531d1f72e407586c21476ef7", "name" : "Group 1"}),
+        new GroupLevel({"_id" : "531d1f72e407586c21476f0c", "name" : "Group 2"}),
+        new GroupLevel({"_id" : "531d1f72e407586c21476f0d", "name" : "Group 3"})
     ]);
 };
