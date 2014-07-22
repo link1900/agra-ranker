5095680786106cd3c7f3f9e076af69f48091af3a
Scott Brown <link1900@gmail.com>
2014-07-22 21:47:54 +1000

    Fixed menu for mobile

--- public/js/controllers/menuCtrl.js
+++ public/js/controllers/menuCtrl.js
@@ -1,5 +1,7 @@
 angular.module('controllers').controller('menuCtrl', ['$scope', 'securityService','$location',
     function($scope, securityService, $location) {
+        $scope.isCollapsed = true;
+
         $scope.logout = function(){
             if($scope.user){
                 securityService.signOut().then(function(){
