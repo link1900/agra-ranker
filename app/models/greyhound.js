7c54d114d977d8c982a6ca91fd3d39066d86327b
Scott Brown <link1900@gmail.com>
2014-09-15 19:01:37 +1000

    Exported Race and Greyhound schemas

--- app/models/greyhound.js
+++ app/models/greyhound.js
@@ -2,13 +2,15 @@ var mongoose = require('mongoose');
 var timestamps = require('mongoose-concrete-timestamps');
 var Schema = mongoose.Schema;
 
+var GreyhoundDesc = {
+    name: { type: String },
+    sireRef: { type: Schema.Types.ObjectId },
+    damRef: { type: Schema.Types.ObjectId }
+};
 
-var GreyhoundSchema = new Schema({
-  name: { type: String },
-  sireRef: { type: Schema.Types.ObjectId },
-  damRef: { type: Schema.Types.ObjectId }
-});
+var GreyhoundSchema = new Schema(GreyhoundDesc);
 
 GreyhoundSchema.plugin(timestamps);
 
 mongoose.model('Greyhound', GreyhoundSchema);
+module.exports = GreyhoundDesc;
--- app/models/race.js
+++ app/models/race.js
@@ -2,14 +2,17 @@ var mongoose = require('mongoose');
 var timestamps = require('mongoose-concrete-timestamps');
 var Schema = mongoose.Schema;
 
-var RaceSchema = new Schema({
+var RaceDesc = {
     name: { type: String },
     date: {type: Date},
     groupLevelRef: {type: Schema.Types.ObjectId},
     distanceMeters: {type: Number},
     disqualified: {type: Boolean}
-});
+};
+
+var RaceSchema = new Schema(RaceDesc);
 
 RaceSchema.plugin(timestamps);
 
 mongoose.model('Race', RaceSchema);
+module.exports = RaceDesc;
