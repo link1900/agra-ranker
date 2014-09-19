0a46388d4e459b132753c01fdefb32245a50c343
Scott Brown <link1900@gmail.com>
2014-09-20 08:29:29 +1000

    Added point allocation functions

--- app/controllers/pointAllotmentController.js
+++ app/controllers/pointAllotmentController.js
@@ -8,6 +8,7 @@ var Placing = mongoose.model('Placing');
 var _ = require('lodash');
 var helper = require('../helper');
 var q = require('q');
+var moment = require('moment');
 
 
 pointAllotmentController.setModel = function(req, res, next, id) {
@@ -69,10 +70,9 @@ pointAllotmentController.clearPointAllotmentsForRankingSystem = function(ranking
 };
 
 pointAllotmentController.allocatePointAllotments = function(rankingSystem){
-    var pointAllocations = _.map(rankingSystem.pointAllotments, function(pointAllotment){
+    var pointAllocations = rankingSystem.pointAllotments.map(function(pointAllotment){
         return pointAllotmentController.allocatePointAllotment(rankingSystem, pointAllotment);
     });
-
     return q.allSettled(pointAllocations).then(function(results){
         console.log(results);
         return results;
@@ -81,24 +81,14 @@ pointAllotmentController.allocatePointAllotments = function(rankingSystem){
 
 pointAllotmentController.allocatePointAllotment = function(rankingSystem, pointAllotment){
     //build query from criteria
-    var query = {};
-    pointAllotment.criteria.forEach(function(criteria){
-        switch (criteria.comparator){
-            case "=":
-                query[criteria.field] = criteria.value;
-                break;
-            default:
-                query[criteria.field] = criteria.value;
-                break;
-        }
-    });
-
+    var query = pointAllotmentController.getQueryForPointAllotment(pointAllotment);
     //run query
     return pointAllotmentController.getPlacingsForAllotment(query).then(function(placings){
         //on each query result insert many pointAllocations
-        var createAllotments = placings.map(function(placing){
+        var points = pointAllotment.points;
+        var createAllotmentPromises = placings.map(function(placing){
             var pointAllotmentRaw = {
-                points: pointAllotment.points,
+                points: points,
                 greyhoundRef: placing.greyhoundRef,
                 placingRef: placing._id,
                 rankingSystemRef: rankingSystem._id
@@ -106,9 +96,7 @@ pointAllotmentController.allocatePointAllotment = function(rankingSystem, pointA
             var pointAllotment = new PointAllotment(pointAllotmentRaw);
             return helper.savePromise(pointAllotment);
         });
-
-        return q.allSettled(createAllotments).then(function(results){
-            console.log(results);
+        return q.allSettled(createAllotmentPromises).then(function(results){
             return results;
         });
     });
@@ -116,7 +104,7 @@ pointAllotmentController.allocatePointAllotment = function(rankingSystem, pointA
 
 pointAllotmentController.getPlacingsForAllotment = function(query){
     var deferred = q.defer();
-    Placing.find(query).exec(function(entities){
+    Placing.find(query).exec(function(err, entities){
         if (err){
             deferred.reject(err);
         } else {
@@ -124,4 +112,72 @@ pointAllotmentController.getPlacingsForAllotment = function(query){
         }
     });
     return deferred.promise;
+};
+
+pointAllotmentController.getQueryForPointAllotment = function(pointAllotment){
+    var query = {};
+    pointAllotment.criteria.forEach(function(criteria){
+        //replace placeholders
+        if (criteria.value != null && _.isString(criteria.value) &&  criteria.value.indexOf('##') == 0){
+            criteria.value = pointAllotmentController.convertPlaceHolder(criteria.value);
+        }
+    });
+
+    pointAllotment.criteria.forEach(function(criteria){
+        switch (criteria.comparator){
+            case "=":
+                query[criteria.field] = criteria.value;
+                break;
+            case ">":
+                pointAllotmentController.addField(query, criteria.field, {"$gt": criteria.value});
+                break;
+            case "<":
+                pointAllotmentController.addField(query, criteria.field, {"$lt": criteria.value});
+                break;
+            case ">=":
+                pointAllotmentController.addField(query, criteria.field, {"$gte": criteria.value});
+                break;
+            case "<=":
+                pointAllotmentController.addField(query, criteria.field, {"$lte": criteria.value});
+                break;
+            case "!=":
+                pointAllotmentController.addField(query, criteria.field, {"$ne": criteria.value});
+                break;
+            default:
+                query[criteria.field] = criteria.value;
+                break;
+        }
+    });
+    return query;
+};
+
+pointAllotmentController.addField = function(query, field, statement){
+    if (query[field]){
+        query[field] = _.extend(query[field], statement);
+    } else {
+        query[field] = statement;
+    }
+};
+
+pointAllotmentController.convertPlaceHolder = function(placeholder){
+    switch (placeholder){
+        case "##currentFinancialYear.start":
+            return pointAllotmentController.getFinancialYearForDate(new Date()).start;
+            break;
+        case "##currentFinancialYear.end":
+            return pointAllotmentController.getFinancialYearForDate(new Date()).end;
+            break;
+        default:
+            return placeholder;
+            break;
+    }
+};
+
+pointAllotmentController.getFinancialYearForDate = function(now){
+    var midYear = moment(now).set('month', 7).set('date', 1).startOf('day');
+    if (midYear.isAfter(now)){
+        return { start: midYear.clone().subtract(12, 'months').toDate(), end : midYear.subtract(1, 'days').endOf('day').toDate()};
+    } else {
+        return { start: midYear.toDate(), end : midYear.clone().add(12, 'months').subtract(1, 'days').endOf('day').toDate()};
+    }
 };
