9af3cc36c214b0112b69e8981163c34be4aa4c52
Scott Brown <link1900@gmail.com>
2014-09-20 20:34:32 +1000

    Added date range and limit to ranking query

--- app/controllers/rankingController.js
+++ app/controllers/rankingController.js
@@ -12,12 +12,27 @@ var q = require('q');
 
 rankingController.getRankings = function(req, res) {
     var rankingSystemRef = req.param('rankingSystemRef');
+    var fromDate = req.param('fromDate');
+    var toDate = req.param('toDate');
+    var limit = req.param('limit');
+    var matchQuery = {};
+    if (fromDate != null){
+        helper.addField(matchQuery,'placing.race.date',{"$gte" : fromDate});
+    }
+    if (toDate != null){
+        helper.addField(matchQuery,'placing.race.date',{"$lte" : toDate});
+    }
+    if (limit == null){
+        limit = 100;
+    }
     if (rankingSystemRef != null){
+        matchQuery['rankingSystemRef']=rankingSystemRef;
         if (/^[0-9a-fA-F]{24}$/.test(rankingSystemRef)){
             var pipeline =[
-            {$match: { 'rankingSystemRef': rankingSystemRef }},
+            {$match: matchQuery},
             {$group: { '_id' : "$placing.greyhoundRef", 'totalPoints' : { '$sum' : "$points" }}},
-            {$sort: {"totalPoints": -1}}
+            {$sort: {"totalPoints": -1}},
+            {$limit: limit}
             ];
             helper.responseFromPromise(res,  helper.aggregatePromise(PointAllotment, pipeline));
         } else {
