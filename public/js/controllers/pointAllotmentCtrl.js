d8c82f1aeb31b57bb9707958c8b8e8d2d3d5602c
Scott Brown <link1900@gmail.com>
2014-09-26 21:51:50 +1000

    Updated point allotment columns with the correct fields

--- public/js/controllers/pointAllotmentCtrl.js
+++ public/js/controllers/pointAllotmentCtrl.js
@@ -1,8 +1,11 @@
 angular.module('controllers').controller('PointAllotmentCtrl',
-    function($scope, $routeParams, headerHelperService, pointAllotmentService, $location) {
+    function($scope, $routeParams, headerHelperService, pointAllotmentService) {
         $scope.pointAllotmentService = pointAllotmentService;
 
         $scope.columnInfo = [
+            {title: "Greyhound", field:"placing.greyhound.name", baseLink:"#/greyhound/view/", linkField: "placing.greyhoundRef", link:true, filter: "uppercase"},
+            {title: "Race", field:"placing.race.name",  baseLink:"#/race/view/", linkField: "placing.raceRef", link:true},
+            {title: "Placing", field:"placing.placing"},
             {title: "Points", field:"points"}
         ];
     }
