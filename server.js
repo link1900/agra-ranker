3f7d095d384a2e86ec07f6556a1daee3f0fae049
Scott Brown <link1900@gmail.com>
2014-11-23 00:24:22 +1100

    Changed server start up

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var serverStarter = module.exports = {};
+var q = require('q');
+var _ = require('lodash');
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
@@ -28,39 +31,56 @@ var db = mongoose.connect(process.env.MONGOHQ_URL);
 //apply migrations
 var migrationDir = path.join(__dirname, '/app/migrations');
 var migrationService = require('./app/migration/migrationService');
-migrationService.applyMigrations(migrationDir).then(function(){
-    // Bootstrap passport config
-    require('./config/passport')(passport);
 
-    var app = express();
+serverStarter.start = _.once(function(){
+    var deferred = q.defer();
 
-    // Express settings
-    require('./config/express')(app, passport, db);
+    migrationService.applyMigrations(migrationDir).then(function(){
+        // Bootstrap passport config
+        require('./config/passport')(passport);
 
-    // Rest routes
-    require('./app/routes.js')(app);
+        var app = express();
+
+        // Express settings
+        require('./config/express')(app, passport, db);
+
+        // Rest routes
+        require('./app/routes.js')(app);
 
 
-    // Start the app by listening on <port>
-    var port = process.env.PORT || 3000;
-    var server = require('http').createServer(app);
+        // Start the app by listening on <port>
+        var port = process.env.PORT || 3000;
+        var server = require('http').createServer(app);
 
-    //var io = require('socket.io')(server);
-    //require('./app/sockets.js').setup(io);
+        //var io = require('socket.io')(server);
+        //require('./app/sockets.js').setup(io);
 
-    server.listen(port);
-    console.log('Express app started on port ' + port);
+        server.on('listening', function(){
+            deferred.resolve(true);
+        });
 
-    //start scheduler
-    var batchService = require('./app/batch/batchService');
-    batchService.startBatchProcessors();
-},function(error){
-    console.log(error);
-    console.error("critical failure apply migration exiting without server start");
-    process.exit(1);
-}).fail(function(err){
-    console.log("failed to start server");
-    console.log(err);
+        server.listen(port);
+        console.log('Express app started on port ' + port);
+
+        //start scheduler
+        var batchService = require('./app/batch/batchService');
+        batchService.startBatchProcessors();
+
+    },function(error){
+        console.log(error);
+        console.error("critical failure apply migration exiting without server start");
+        process.exit(1);
+    }).fail(function(err){
+        console.log("failed to start server");
+        console.log(err);
+        process.exit(1);
+    });
+    return deferred.promise;
 });
+
+serverStarter.start();
+
+
+
 
 
