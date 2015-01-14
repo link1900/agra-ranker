06017d9c2484671fffb7900c96fe62d687d26f56
Scott Brown <link1900@gmail.com>
2015-01-14 13:56:24 +1100

    Removed unused import

--- app/greyhound/greyhoundService.js
+++ app/greyhound/greyhoundService.js
@@ -2,7 +2,6 @@ var greyhoundService = module.exports = {};
 
 var mongoose = require('mongoose');
 var Greyhound = require('./greyhound').model;
-var Placing = require('../placing/placing').model;
 var BatchResult = require('../batch/batchResult').model;
 var batchService = require('../batch/batchService');
 var fileService = require('../file/fileService');
