7f63c3430aac78c9989d4e578c64eb8944a8e97a
Scott Brown <link1900@gmail.com>
2015-02-14 19:30:35 +1100

    Added addtional option to filter field

--- public/ranker/filters/fromNow.js
+++ public/ranker/filters/fromNow.js
@@ -4,6 +4,9 @@ angular.module('filters').filter('fromNow', function() {
             case 'both':
                 return moment(input).fromNow() + " (" + moment(input).format('MMM D, YYYY h:mm:ss A') + ")";
                 break;
+            case 'bothMed':
+                return moment(input).format('D MMM YYYY h:mm:ss A') + " - " + moment(input).fromNow();
+                break;
             default:
                 return moment(input).fromNow();
                 break;
