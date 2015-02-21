4c51fa9d77c665babeb2b36e6bd6ebce88677600
Scott Brown <link1900@gmail.com>
2015-02-21 13:28:55 +1100

    Added more description to event log page

--- public/ranker/event/eventCtrl.js
+++ public/ranker/event/eventCtrl.js
@@ -11,9 +11,17 @@ angular.module('controllers').controller('eventCtrl', function($scope,
 
         $scope.postProcess = function(events){
             _.each(events, function(event){
-                if (event != null && event.data != null && event.data.entity != null){
-                    if (event.data.entity.name != null){
-                        event.description = event.data.entity.name;
+                if (event != null && event.data != null){
+                    if (event.data.entity != null){
+                        if (event.data.entity.name != null){
+                            event.description = event.data.entity.name;
+                        }
+                        if (event.description == null && event.data.entity.greyhound != null){
+                            event.description = event.data.entity.placing + " - " + event.data.entity.greyhound.name;
+                        }
+                    }
+                    if (event.description == null && event.data.rankingsCalculated != null){
+                        event.description = "Calculated " + event.data.rankingsCalculated + " rankings";
                     }
                 }
             });
