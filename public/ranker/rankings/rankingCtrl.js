25ff01607de1573ed0ba0d4562779ab40be10bb8
Scott Brown <link1900@gmail.com>
2015-02-22 14:44:06 +1100

    Started front end controls for the rankings

--- public/ranker/rankings/rankingCtrl.js
+++ public/ranker/rankings/rankingCtrl.js
@@ -1,9 +1,28 @@
-angular.module('controllers').controller('rankingCtrl', function($scope, rankingSvr) {
+angular.module('controllers').controller('rankingCtrl', function($scope, rankingSvr, rankingSystemSvr) {
     $scope.rankingService = rankingSvr;
-
+    $scope.selected = {};
     $scope.columnInfo = [
         {title: "Rank", field:"rank"},
         {title: "Greyhound", field:"greyhoundName"},
         {title: "Points", field:"totalPoints"}
     ];
+    $scope.rankingSystems = [];
+    $scope.rankingSystemSearch = {
+        page : 1,
+        per_page : 15,
+        like : '',
+        sort_field: 'name',
+        sort_direction: 'asc'
+    };
+
+    $scope.loadRankingSystems = function(){
+        rankingSystemSvr.query($scope.rankingSystemSearch, function(resultModels) {
+            console.log(resultModels);
+            $scope.rankingSystems = resultModels;
+            $scope.selected.rankingSystemRef = $scope.rankingSystems[0]._id;
+        });
+    };
+
+    $scope.loadRankingSystems();
+
 });
