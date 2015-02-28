22db62db21b3fad97e4480a9bf63a4a1cc2f20be
Scott Brown <link1900@gmail.com>
2015-02-28 13:07:31 +1100

    Changed ranking system schema to not use sub schemas to define default arrays

--- app/ranking/rankingSystem.js
+++ app/ranking/rankingSystem.js
@@ -4,26 +4,26 @@ var mongoose = require('mongoose');
 var timestamps = require('mongoose-concrete-timestamps');
 var Schema = mongoose.Schema;
 
-var allotmentCriteriaSchema = new Schema({
+var allotmentCriteriaDefinition = {
     field: {type: String},
     comparator:{type: String},
     value:{type: Schema.Types.Mixed},
     type:{type: String}
-});
+};
 
-var pointAllotmentSchema = new Schema({
-    criteria: {type: [allotmentCriteriaSchema]},
+var pointAllotmentDefinition = {
+    criteria: {type: [allotmentCriteriaDefinition], default: []},
     series: {type: String},
     points: {type: Number}
-});
+};
 
 rankingSystem.schema = new Schema({
     name: { type: String },
     description: {type: String},
     equalPositionResolution: {type: String, default: 'splitPoints'},
     defaultRanking: {type: Boolean, default: false},
-    pointAllotments: {type: [pointAllotmentSchema]},
-    commonCriteria : {type:[allotmentCriteriaSchema]}
+    pointAllotments: {type: [pointAllotmentDefinition], default : []},
+    commonCriteria : {type:[allotmentCriteriaDefinition], default: []}
 });
 
 rankingSystem.schema.plugin(timestamps);
