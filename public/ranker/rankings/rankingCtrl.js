843c0196fcd63dd368ce92c8ed1b7194ae5bb091
Scott Brown <link1900@gmail.com>
2015-03-24 20:46:59 +1100

    Changed the ranking ctrl year selector over to the new dateRange field type

--- public/ranker/rankings/rankingCtrl.js
+++ public/ranker/rankings/rankingCtrl.js
@@ -16,37 +16,9 @@ angular.module('controllers').controller('rankingCtrl', function($scope, ranking
     };
 
     $scope.searchInfo = [
-        {"name":"Year", fieldStart:"startDate", fieldEnd:"endDate", selectedStart: "start", selectedEnd:"end", type:"selectRangeSingle",  options: [], loadOptions: function(){
-            var startYear = 2010;
-            var endYear = new Date().getFullYear();
-            var periods = [];
-            for (var i=endYear; i>=startYear;i--){
-                periods.push({name: i+"", _id: $scope.getYearDates(i)});
-                //periods.push({name: i+"-"+(i+1), period: $scope.getFYearDates(i)});
-            }
-            return $q.when(periods);
-        }},
+        {"name":"Ranking for period", type:"dateRange"},
         {"name":"Type", field:"rankingSystemRef", type:"select", options: [], loadOptions: function(){
             return rankingSystemSvr.query($scope.rankingSystemSearch).$promise;
         }}
     ];
-
-    $scope.getFYearDates = function(year){
-        var midYear = moment().set('year', year).set('month', 'July').set('date', 1).startOf('day');
-        return { start: midYear.toDate(), end : midYear.clone().add(12, 'months').subtract(1, 'days').endOf('day').toDate()};
-    };
-
-    $scope.getYearDates = function(year){
-        var startYear = moment().set('year',year).set('month', 'Jan').set('date', 1).startOf('day');
-        var endYear = moment().set('year',year).set('month', 'Dec').set('date', 31).endOf('day');
-        return {start: startYear.toString(), end: endYear.toString()};
-    };
-
-    $scope.loadRankingSystems = function(){
-        rankingSystemSvr.query($scope.rankingSystemSearch, function(resultModels) {
-            $scope.searchInfo[1].options = resultModels;
-            $scope.searchInfo[1].selected = $scope.searchInfo[1].options[0]._id;
-            console.log("i updated the selection", $scope.searchInfo[1].selected);
-        });
-    };
 });
