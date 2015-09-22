71e373593e9d617d27a5bac81e40d6fa924f1162
Scott Brown <link1900@gmail.com>
2015-09-22 21:32:14 +1000

    Updated the greyhoundSvr findOrCreate

--- public/ranker/greyhound/greyhoundSvr.js
+++ public/ranker/greyhound/greyhoundSvr.js
@@ -11,15 +11,19 @@ angular.module('services').factory('greyhoundSvr', ['$resource', '$http',
             }
         );
 
-        service.findOrCreateGreyhound = function(newGreyhound){
-            return service.query(newGreyhound).$promise.then(function(results){
+        service.findOrCreateGreyhound = function(newGreyhoundName){
+            return service.query({name: newGreyhoundName}).$promise.then(function(results){
                 if (results.length != null){
                     if (results.length > 0){
                         return results[0];
                     } else  {
                         //cannot find anything create a new one
-                        return $http.post("/greyhound", newGreyhound).then(function(creationResult){
-                            return creationResult.data;
+                        return $http.post("/greyhound", {name: newGreyhoundName}).then(function(creationResult){
+                            if (creationResult != null){
+                                return creationResult.data;
+                            } else {
+                                return null;
+                            }
                         });
                     }
                 }
