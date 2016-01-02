084b89eb560060b44b32cb598b8ce7e90c7c70e8
Scott Brown <link1900@gmail.com>
2016-01-03 00:11:59 +1100

    Cleaned up the grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -2,27 +2,6 @@ module.exports = function(grunt) {
     // Project Configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            all: {
-                src: ['server.js', 'app/**/*.js', 'public/js/**', 'test/**/*.js'],
-                options: {
-                    jshintrc: true
-                }
-            }
-        },
-        'string-replace': {
-            version: {
-                files: {
-                    'public/views/footer.html': 'templates/public/views/footer.html'
-                },
-                options: {
-                    replacements: [{
-                        pattern: /{{VERSION}}/g,
-                        replacement: '<%= pkg.version %>'
-                    }]
-                }
-            }
-        },
         includeSource: {
             options :{
                 basePath: 'public',
@@ -37,14 +16,12 @@ module.exports = function(grunt) {
     });
 
     //Load NPM tasks
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-include-source');
 
     //Making grunt default to force in order not to break the project.
     grunt.option('force', true);
 
-    grunt.registerTask('build', ['includeSource','string-replace:version']);
+    grunt.registerTask('build', ['includeSource']);
 
     //Default task(s).
     grunt.registerTask('default', ['build']);
