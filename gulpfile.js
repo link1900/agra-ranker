9d5ae8a65ad1673dec4cfc9ba7a165b435b42a57
Scott Brown <link1900@gmail.com>
2016-01-16 13:54:54 +1100

    Changed over to gulp completely for dev running. Added live reload and gulp and nodemon watch files correctly now.

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,73 @@
+'use strict';
+
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
 var concat = require('gulp-concat');
-var templateCache = require('gulp-angular-templatecache');
-var gulpSequence = require('gulp-sequence');
-var del = require('del');
 
-var paths = {
-    scripts: ['client/ranker/**/*.js'],
-    html: ['client/ranker/**/*.html','client/views/*.html'],
-    copy: [
-        'client/lib/**/*',
-        'client/js/**/*',
-        'client/template/**/*',
-        'client/css/**/*',
-        'client/img/**/*',
-        'client/ranker/**/*.html',
-        'client/views/**/*',
-        'client/index.html',
-        'client/ranker/**/*.js'
-    ]
-};
+// we'd need a slight delay to reload browsers
+// connected to browser-sync after restarting nodemon
+var BROWSER_SYNC_RELOAD_DELAY = 500;
 
-gulp.task('clean', function() {
-    return del(['dist']);
+gulp.task('nodemon', function (cb) {
+    var called = false;
+    return nodemon({
+
+        // nodemon our expressjs server
+        script: 'server.js',
+
+        // watch core server file(s) that require server restart on change
+        watch: ['server.js', 'app/**/*.js']
+    })
+        .on('start', function onStart() {
+            // ensure start only got called once
+            if (!called) { cb(); }
+            called = true;
+        })
+        .on('restart', function onRestart() {
+            // reload connected browsers after a slight delay
+            setTimeout(function reload() {
+                browserSync.reload({
+                    stream: false
+                });
+            }, BROWSER_SYNC_RELOAD_DELAY);
+        });
 });
 
-gulp.task('html', function () {
-    return gulp.src(paths.html)
-        .pipe(templateCache())
-        .pipe(gulp.dest('dist'));
+gulp.task('browser-sync', ['nodemon'], function () {
+
+    // for more browser-sync config options: http://www.browsersync.io/docs/options/
+    browserSync({
+
+        // informs browser-sync to proxy our expressjs app which would run at the following location
+        proxy: 'http://localhost:3000',
+
+        // informs browser-sync to use the following port for the proxied app
+        // notice that the default port is 3000, which would clash with our expressjs
+        port: 4000,
+
+        // open the proxied app in chrome
+        browser: ['google-chrome']
+    });
 });
 
-gulp.task('scripts', function() {
-    return gulp.src(paths.scripts)
+gulp.task('js',  function () {
+    return gulp.src('client/ranker/**/*.js')
         .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('client'));
 });
 
-gulp.task('copy', function(){
-    return gulp.src(paths.copy,{"base": "client"}).pipe(gulp.dest('dist'));
+gulp.task('css', function () {
+    return gulp.src('client/**/*.css')
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('build', gulpSequence('clean', 'copy', 'scripts'));
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch('client/**/*.js',   ['js', browserSync.reload]);
+    gulp.watch('client/**/*.css',  ['css']);
+    gulp.watch('client/**/*.html', ['bs-reload']);
+});
