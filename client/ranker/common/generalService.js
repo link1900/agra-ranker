5a1025c6e0dec4ffba9246d692030aba5b548691
Scott Brown <link1900@gmail.com>
2016-01-26 00:22:42 +1100

    Added helpers to general service

--- client/ranker/common/generalService.js
+++ client/ranker/common/generalService.js
@@ -7,5 +7,20 @@ angular.module('services').service('generalService',  function($http) {
         });
     };
 
+    generalService.getYearArray = function(fromYear){
+        var endYear = new Date().getFullYear();
+        var years = [];
+        for (var i=endYear; i>=fromYear;i--){
+            years.push({name:i+"",value:i});
+        }
+        return years;
+    };
+
+    generalService.getYearDates = function(year){
+        var startYear = moment().set('year',year).set('month', 'Jan').set('date', 1).startOf('day');
+        var endYear = moment().set('year',year).set('month', 'Dec').set('date', 31).endOf('day');
+        return {start: startYear.toDate(), end: endYear.toDate()};
+    };
+
     return generalService;
 });
