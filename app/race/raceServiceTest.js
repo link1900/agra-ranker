bbf31ecb7e7045303b2f8518e8233b50f415695f
Scott Brown <link1900@gmail.com>
2016-02-22 19:39:50 +1100

    Added placeholder tests for raceService

---
+++ app/race/raceServiceTest.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe("raceService", function() {
+    describe("validateRace", function(){
+        it("should pass when is a valid racae", function (done) {
+            assert(true);
+            done();
+        });
+    });
+});
