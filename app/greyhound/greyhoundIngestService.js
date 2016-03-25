e82564614ed99e2bf93cbf9b7bc3f00e14e9f68a
Scott Brown <link1900@gmail.com>
2016-03-25 17:10:33 +1100

    Added ref to the external info lookup

--- app/greyhound/greyhoundIngestService.js
+++ app/greyhound/greyhoundIngestService.js
@@ -3,6 +3,7 @@ var greyhoundIngestService = module.exports = {};
 var q = require('q');
 var Xray = require('x-ray');
 var xray = Xray();
+var logger = require('winston');
 
 greyhoundIngestService.grvUrl = "https://fasttrack.grv.org.au";
 
@@ -10,13 +11,13 @@ greyhoundIngestService.getExternalGreyhoundInfo = function(greyhound){
     if (greyhound && greyhound.name){
         var url = greyhoundIngestService.buildGRVGreyhoundUrl(greyhound.name);
         var selector = {
-            id: "a[rel='dog-details']@href",
+            url: "a[rel='dog-details']@href",
             sireName: "a[rel='View-Sire']",
             damName: "a[rel='View-Dam']"
         };
-        return greyhoundIngestService.downloadInfo(url, selector).then(greyhoundIngestService.grvPostProcess);
+        return greyhoundIngestService.downloadInfo(url, selector).then(greyhoundIngestService.addRef);
     } else {
-        q.reject("cannot retieve info on invalid greyhound or greyhound name")
+        q.reject("cannot retrieve info on invalid greyhound or greyhound name")
     }
 };
 
@@ -24,18 +25,20 @@ greyhoundIngestService.buildGRVGreyhoundUrl = function(name){
     return greyhoundIngestService.grvUrl +"/Dog/Search?Dog=" + encodeURI(name) + "&ExactMatch=true&Search=Search";
 };
 
-greyhoundIngestService.grvPostProcess = function(result){
-    result.source = "fasttrack";
-    if (result.id){
-        result.url = result.id;
-        result.id = result.id.replace('https://fasttrack.grv.org.au/Dog/Details/-','');
+greyhoundIngestService.addRef = function(result){
+    var ref = {};
+    ref.source = "fasttrack";
+    if (result.url){
+        ref.url = result.url;
+        ref.id = result.id.replace('https://fasttrack.grv.org.au/Dog/Details/','');
     }
-
+    result.ref = ref;
     return q(result);
 };
 
 greyhoundIngestService.downloadInfo = function(url, selector){
     var deferred = q.defer();
+    logger.info("API request [" + url + "]");
     xray(url, selector)(function(err, result) {
         if(err){
             deferred.reject(err);
