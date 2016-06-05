cfb1a39dd5e35f75b7906cba30e477f7fbea36a7
Scott Brown <link1900@gmail.com>
2016-06-05 10:00:07 +1000

    Fixed and issue with the lookup not finding a greyhounds name when it has been used before

--- app/greyhound/greyhoundIngestService.js
+++ app/greyhound/greyhoundIngestService.js
@@ -12,8 +12,7 @@ greyhoundIngestService.grvUrl = "https://fasttrack.grv.org.au";
 greyhoundIngestService.getExternalGreyhoundInfo = function(greyhoundName){
     if (greyhoundName){
         var url = greyhoundIngestService.buildGRVGreyhoundUrl(greyhoundName);
-        return greyhoundIngestService.downloadInfo(url)
-            .then(greyhoundIngestService.postProcess);
+        return greyhoundIngestService.downloadGreyhoundInfo(url);
     } else {
         q.reject("cannot retrieve info on invalid greyhound or greyhound name")
     }
@@ -23,61 +22,122 @@ greyhoundIngestService.buildGRVGreyhoundUrl = function(name){
     return greyhoundIngestService.grvUrl +"/Dog/Search?Dog=" + encodeURI(name) + "&ExactMatch=true&Search=Search";
 };
 
-greyhoundIngestService.postProcess = function(scrapResult){
-    var fields = scrapResult.fields.map(function(field){
-        field.value = field.value.replace(/\n/g, '').trim();
-        return field;
-    });
-    var postProcess = {};
+greyhoundIngestService.postProcessGreyhoundScrap = function(scrapResult){
+    logger.info("lookup is post processing found results");
+    if (scrapResult && scrapResult.fields && scrapResult.fields.length > 0){
+        var fields = scrapResult.fields.map(function(field){
+            field.value = field.value.replace(/\n/g, '').trim();
+            return field;
+        });
+        var postProcess = {};
 
-    postProcess.status = _.get(_.find(fields, { 'label': 'Status'}), 'value', null);
-    postProcess.color = _.get(_.find(fields, { 'label': 'Colour'}), 'value', null);
-    postProcess.gender = _.get(_.find(fields, { 'label': 'Sex'}), 'value', null);
-    postProcess.dateOfBirth = _.get(_.find(fields, { 'label': 'Whelped'}), 'value', null);
-    postProcess.sireName = _.get(_.find(fields, { 'label': 'Sire'}), 'value', null);
-    postProcess.damName = _.get(_.find(fields, { 'label': 'Dam'}), 'value', null);
-    if (postProcess.gender){
-        postProcess.gender = postProcess.gender.toLowerCase();
+        postProcess.status = _.get(_.find(fields, { 'label': 'Status'}), 'value', null);
+        postProcess.color = _.get(_.find(fields, { 'label': 'Colour'}), 'value', null);
+        postProcess.gender = _.get(_.find(fields, { 'label': 'Sex'}), 'value', null);
+        postProcess.dateOfBirth = _.get(_.find(fields, { 'label': 'Whelped'}), 'value', null);
+        postProcess.sireName = _.get(_.find(fields, { 'label': 'Sire'}), 'value', null);
+        postProcess.damName = _.get(_.find(fields, { 'label': 'Dam'}), 'value', null);
+        if (postProcess.gender){
+            postProcess.gender = postProcess.gender.toLowerCase();
+        }
+
+        if (postProcess.dateOfBirth){
+            postProcess.dateOfBirth = moment.tz(postProcess.dateOfBirth, 'DD/MM/YYYY', 'Australia/Melbourne').toDate();
+        }
+
+        postProcess.nameAssignedAt = _.get(_.find(fields, { 'label': 'Name Assigned'}), 'value', null);
+        if (postProcess.nameAssignedAt){
+            postProcess.nameAssignedAt = moment.tz(postProcess.nameAssignedAt, 'DD/MM/YYYY', 'Australia/Melbourne').toDate();
+        }
+
+        var ref = {};
+        ref.source = "fasttrack";
+        if (scrapResult.url){
+            ref.id = scrapResult.url.replace('https://fasttrack.grv.org.au/Dog/Details/', '');
+            ref.url = scrapResult.url;
+        }
+        postProcess.ref = ref;
+        return q(postProcess);
+    } else {
+        return q(scrapResult);
     }
-
-    if (postProcess.dateOfBirth){
-        postProcess.dateOfBirth = moment.tz(postProcess.dateOfBirth, 'DD/MM/YYYY', 'Australia/Melbourne').toDate();
-    }
-
-    var ref = {};
-    ref.source = "fasttrack";
-    if (scrapResult.url){
-        ref.id = scrapResult.url.replace('/Dog/Details/', '');
-        ref.url = 'https://fasttrack.grv.org.au/Dog/Details/' + ref.id;
-    }
-    postProcess.ref = ref;
-
-    return q(postProcess);
 };
 
-greyhoundIngestService.addRef = function(result){
-    var ref = {};
-    ref.source = "fasttrack";
-    if (result.url){
-        ref.url = result.url;
-        ref.id = result.id.replace('https://fasttrack.grv.org.au/Dog/Details/','');
-    }
-    result.ref = ref;
-    return q(result);
+greyhoundIngestService.downloadGreyhoundInfo = function(url){
+    return greyhoundIngestService.downloadGreyhoundFromSite(url)
+        .then(greyhoundIngestService.processMultiples)
+        .then(greyhoundIngestService.postProcessGreyhoundScrap);
 };
 
-greyhoundIngestService.downloadInfo = function(url){
+greyhoundIngestService.downloadGreyhoundFromSite = function(url){
     var deferred = q.defer();
-    logger.info("API request [" + url + "]");
+    //logger.info("Scrap site [" + url + "]");
     xray(url, {
         url: "a[rel='dog-details']@href",
+        searchResults: xray(".search-results", {
+            results: xray("tr", [{
+                name: "a",
+                url: "a@href"
+            }])
+        }),
         fields: xray('.field', [{
             label: 'label',
             value: ".display-value"
         }])
     })(function(err, result) {
-        if (err) throw deferred.reject(err);
-        deferred.resolve(result);
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(result);
+        }
+
     });
     return deferred.promise;
+};
+
+greyhoundIngestService.processMultiples = function(scrapResult){
+    if (scrapResult.url){
+        logger.info("lookup found valid greyhound");
+        return q(scrapResult);
+    } else if (!scrapResult.url && scrapResult.searchResults && scrapResult.searchResults.results){
+        logger.info("lookup found multiple greyhounds", scrapResult.searchResults.results.length);
+        return greyhoundIngestService.scrapMultiples(scrapResult.searchResults.results);
+    } else {
+        logger.info("lookup found nothing", JSON.stringify(scrapResult));
+        return q(scrapResult);
+    }
+};
+
+greyhoundIngestService.getYoungest = function(lookups){
+    logger.info("lookup finding youngest valid greyhound from: " + JSON.stringify(lookups));
+
+    if (lookups && lookups.length > 0){
+        //first remove retired greyhounds or anything without name assigned date
+        lookups = lookups.filter(function(lookup){
+            return lookup.status !== "Retired" && lookup.nameAssignedAt != null;
+        });
+
+        //sort by nameAssignedAt desending
+        lookups.sort(function(a,b) { return new Date(b.nameAssignedAt).getTime() - new Date(a.nameAssignedAt).getTime() } );
+
+        _.last(lookups);
+    } else {
+        return q(null);
+    }
+
+    return q(lookups[0]);
+};
+
+greyhoundIngestService.scrapMultiples = function(multipleGreyhounds){
+    if (multipleGreyhounds && multipleGreyhounds.length > 0){
+        return q.all(multipleGreyhounds.map(function(greyhoundSearch){
+            var lookupUrl = greyhoundSearch.url;
+            return greyhoundIngestService.downloadGreyhoundInfo(lookupUrl);
+        })).then(function(lookupResults){
+            return greyhoundIngestService.getYoungest(lookupResults);
+        });
+    } else {
+        logger.info("lookup tried to lookup mulitple greyhounds but failed");
+        return q.resolve(null);
+    }
 };
--- scripts/testRead.js
+++ scripts/testRead.js
@@ -3,12 +3,15 @@ var fs = require('fs');
 var greyhoundIngestService = require('../app/greyhound/greyhoundIngestService');
 
 
-fs.readFile('./scripts/testDuplicate.html', {encoding: 'utf8'}, function(err, data) {
-    if (err) throw err;
-    return greyhoundIngestService.downloadGreyhoundInfo(data).then(function(result){
-        console.log("results",JSON.stringify(result));
-    }, function(error){
-        console.error(error);
-        throw error;
-    })
-});
+//fs.readFile('./scripts/testDuplicate.html', {encoding: 'utf8'}, function(err, data) {
+//    if (err) throw err;
+//    return greyhoundIngestService.downloadGreyhoundInfo(data).then(function(result){
+//        console.log("results",JSON.stringify(result));
+//    }, function(error){
+//        console.error(error);
+//        throw error;
+//    })
+//});
+
+
+greyhoundIngestService.getExternalGreyhoundInfo("DEMON BALE").then(console.log, console.log);
