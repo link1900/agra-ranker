efa527efbf7819d1b147a8e3c914cf0fbe37e787
Scott Brown <link1900@gmail.com>
2017-06-13 22:36:40 +1000

    Added more models to the dataloader

--- app/data/rankerDataLoaderHelper.js
+++ app/data/rankerDataLoaderHelper.js
@@ -1,13 +1,12 @@
 import { createMongoLoader, createLoaderForMongooseModel } from '../common/mongoDataLoaderHelper';
 
 /* eslint global-require: 0 */
-export const modelTypes = {
-    placing: require('../placing/placing').model
-};
-
 export function createLoaders() {
     const mongoLoader = createMongoLoader();
     return {
-        placing: createLoaderForMongooseModel(mongoLoader, require('../placing/placing').model)
+        greyhound: createLoaderForMongooseModel(mongoLoader, require('../greyhound/greyhound').model),
+        race: createLoaderForMongooseModel(mongoLoader, require('../race/race').model),
+        placing: createLoaderForMongooseModel(mongoLoader, require('../placing/placing').model),
+        rankingSystem: createLoaderForMongooseModel(mongoLoader, require('../ranking/rankingSystem').model)
     };
 }
