947078ac84af3696a6bdc433d8c5a1fffa313f8e
Scott Brown <link1900@gmail.com>
2018-07-30 00:06:04 +1000

    Updated auth0 libs to use the latest versions.

--- client/js/core.js
+++ client/js/core.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef,no-console */
 angular.module('core',
-    ['auth0.lock', 'angular-jwt', 'ngRoute',
+    ['auth0.auth0', 'angular-jwt', 'ngRoute',
         'ngResource',
         'ngSanitize',
         'ui.bootstrap',
@@ -18,10 +19,14 @@ angular.module('core',
         'filters',
         'ui.select']).config(config).run(run);
 
-function config($httpProvider, lockProvider, jwtOptionsProvider) {
-    lockProvider.init({
-        clientID: 'dH125kyqeBr6M0KBt5fJckXiUAi2x7BS',
-        domain: 'app23098245.auth0.com'
+function config($httpProvider, $locationProvider, angularAuth0Provider, jwtOptionsProvider) {
+    angularAuth0Provider.init({
+        clientID: envConfig.AUTH0_CLIENT_ID,
+        domain: envConfig.AUTH0_DOMAIN,
+        audience: envConfig.AUTH0_AUDIENCE,
+        redirectUri: envConfig.AUTH0_CALLBACK_URL,
+        responseType: 'token id_token',
+        scope: 'openid'
     });
 
     jwtOptionsProvider.config({
@@ -33,10 +38,54 @@ function config($httpProvider, lockProvider, jwtOptionsProvider) {
     $httpProvider.interceptors.push('jwtInterceptor');
 }
 
-function run(lock, authService, authManager){
-    lock.interceptHash();
-    authService.registerAuthenticationListener();
-    authManager.checkAuthOnRefresh();
+function checkSession($rootScope) {
+    var existingToken = localStorage.getItem('id_token');
+    var expiresAt = localStorage.getItem('expires_at');
+    if (existingToken && expiresAt > new Date().getTime()) {
+        $rootScope.user = true;
+    }
+}
+
+function interceptHash($rootScope, $location) {
+    if (typeof auth0.WebAuth !== 'function') {
+        throw new Error('Auth0.js version 8 or higher must be loaded');
+    }
+
+    $rootScope.$on('$locationChangeStart', function(event, location) {
+        if (
+            /id_token=/.test(location) ||
+            /access_token=/.test(location) ||
+            /error=/.test(location)
+        ) {
+            var webAuth = new auth0.WebAuth({
+                clientID: envConfig.AUTH0_CLIENT_ID,
+                domain: envConfig.AUTH0_DOMAIN
+            });
+
+            var hash = $location.hash() || window.location.hash;
+
+            webAuth.parseHash(
+                { hash: hash, _idTokenVerification: true },
+                function(err, authResult) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    if (authResult && authResult.idToken) {
+                        let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+                        localStorage.setItem('access_token', authResult.accessToken);
+                        localStorage.setItem('id_token', authResult.idToken);
+                        localStorage.setItem('expires_at', expiresAt);
+                        $rootScope.user = true;
+                    }
+                }
+            );
+        }
+    });
+}
+
+function run($rootScope, $location, angularAuth0) {
+    checkSession($rootScope);
+    interceptHash($rootScope, $location, angularAuth0);
 }
 
 angular.module('controllers', []);
--- client/ranker/common/authService.js
+++ client/ranker/common/authService.js
@@ -6,51 +6,26 @@
         .module('services')
         .service('authService', authService);
 
-    authService.$inject = ['$rootScope', 'lock', 'authManager'];
+    authService.$inject = ['$rootScope', 'angularAuth0'];
 
-    function authService($rootScope, lock, authManager) {
+    function authService($rootScope, angularAuth0) {
 
-        var userProfile = JSON.parse(localStorage.getItem('profile'));
 
         function login() {
-            lock.show();
+            angularAuth0.authorize();
         }
 
-        // Logging out just requires removing the user's
-        // id_token and profile
         function logout() {
             localStorage.removeItem('id_token');
-            localStorage.removeItem('profile');
-            authManager.unauthenticate();
-            userProfile = {};
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('expires_at');
+            $rootScope.user = undefined;
         }
 
-        // Set up the logic for when a user authenticates
-        // This method is called from app.run.js
-        function registerAuthenticationListener() {
-            if (userProfile && authManager.isAuthenticated()){
-                $rootScope.user = userProfile;
-                $rootScope.$broadcast('userProfileSet', userProfile);
-            }
-
-            lock.on('authenticated', function(authResult) {
-                localStorage.setItem('id_token', authResult.idToken);
-                authManager.authenticate();
-                lock.getProfile(authResult.idToken, function(error, profile) {
-                    if (!error) {
-                        localStorage.setItem('profile', JSON.stringify(profile));
-                        $rootScope.user = profile;
-                        $rootScope.$broadcast('userProfileSet', profile);
-                    }
-                });
-            });
-        }
 
         return {
-            userProfile: userProfile,
             login: login,
-            logout: logout,
-            registerAuthenticationListener: registerAuthenticationListener
+            logout: logout
         }
     }
 })();
